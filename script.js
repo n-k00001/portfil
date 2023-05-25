d19f2f9fb426f6025ef59f6dc7713f560b2b8045
n_k00001 <nooorykhalef@gmail.com>
2023-05-26 01:29:30 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,29 @@
+//Icon of navbar
+function display(id, word) {
+    document.getElementById(id).style.display = "none";
+    document.getElementById(word).setAttribute("style", "display: block !important; left:0; ");
+}
+
+let navList = document.getElementsByClassName('nav-item');
+/*navabar activation*/
+window.onscroll = function () {
+
+    navList.forEach(nav => {
+        if(nav.firstChild.classList.contains('active'))
+            nav.classList.add('active');
+        else nav.classList.remove('active');
+    });
+}
+
+// card project hover
+function change(id){
+    let card  = document.getElementById(id)
+    card.style.backgroundImage = 'none'
+    card.style.backgroundColor = "#60adec"
+}
+function discard(id){
+    let card  = document.getElementById(id)
+    card.style.backgroundImage = `url('/imgs/${id}.png')`
+}
+
+
